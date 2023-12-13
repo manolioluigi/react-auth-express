7bda809d27058c49b5420ad6f7e3890a7055f67d
manolioluigi <luigi.manolio1999@gmail.com>
2023-12-13 14:21:50 +0100

    creazione progetto

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div className="height-77">
+            <div className="container">
+                <h2>Dashboard</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
